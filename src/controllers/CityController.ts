a24fae5aacdf443538f07abdc83513fea74dee59
lucashfs <lucash.silva1612@gmail.com>
2020-07-08 17:28:47 -0300

    Creating Basic Routes & Controllers

---
+++ src/controllers/CityController.ts
@@ -0,0 +1,28 @@
+import knex from '../database/connection';
+import { Request, Response } from 'express';
+
+class CityController {
+
+  async index( req: Request, res: Response ) {
+    return res.json({msg: 'index'});
+  }
+
+  async create( req: Request, res: Response ) {
+    //validate data
+
+    return res.json({msg: 'create'});
+  }
+
+  async update( req: Request, res: Response ) {
+    //validate data
+
+    return res.json({msg: 'update'});
+  }
+
+  async delete( req: Request, res: Response ) {
+    return res.json({msg: 'delete'});
+  }
+
+}
+
+export default CityController;
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,47 @@
 import express from 'express';
 
-const routes = express.Router();
+import ProductController from './controllers/ProductController';
+import ProviderController from './controllers/ProviderController';
+import CityController from './controllers/CityController';
 
-routes.get('/');
+const routes = express.Router();
+const productController = new ProductController();
+const providerController = new ProviderController();
+const cityController = new CityController();
+
+
+// !-------   Start of Basic Cruds   -------!
+
+// Product
+routes.get('/products', productController.index);
+routes.post('/products', productController.create);
+routes.put('/products/:id', productController.update);
+routes.delete('/products/:id', productController.delete);
+
+
+// Provider
+routes.get('/providers', providerController.index);
+routes.post('/providers', providerController.create);
+routes.put('/providers/:id', providerController.update);
+routes.delete('/providers/:id', providerController.delete);
+
+
+// City
+routes.get('/cities', cityController.index);
+routes.post('/cities', cityController.create);
+routes.put('/cities/:id', cityController.update);
+routes.delete('/cities/:id', cityController.delete);
+
+
+// Todo: Category
+
+
+// Todo: Transporter
+
+
+// Todo: Store
+
+
+// !-------   End of Basic Cruds   -------!
 
 export default routes;
