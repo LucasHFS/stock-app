62e368e2570bbd2f9d5630a14569967f1a26a762
Vinicius Correia <softmakerinfos@gmail.com>
2020-07-09 17:19:36 -0300

    migrations

--- src/database/migrations/004_create_product.ts
+++ src/database/migrations/004_create_product.ts
@@ -5,7 +5,18 @@ export async function up(knex: Knex){
   return knex.schema.createTable('product', table => {
     table.increments('id').primary();
     table.string('description').notNullable();
-    
+    table.string('weight').notNullable();
+    table.integer('quantity').notNullable();
+
+    table.integer('category_id')
+    .notNullable()
+    .references('id')
+    .inTable('category');
+
+    table.integer('provider_id')
+    .notNullable()
+    .references('id')
+    .inTable('provider');
     
   });
 }
