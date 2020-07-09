1c98e46762c9d8bd3e1bec069f11649b3e068937
Vinicius Correia <softmakerinfos@gmail.com>
2020-07-09 16:54:29 -0300

    migrations

--- src/database/migrations/004_create_product.ts
+++ src/database/migrations/004_create_product.ts
@@ -4,6 +4,8 @@ import Knex from 'knex';
 export async function up(knex: Knex){
   return knex.schema.createTable('product', table => {
     table.increments('id').primary();
+    table.string('description').notNullable();
+    
     
   });
 }
